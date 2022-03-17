5961dcb97c17b24d03fc6f8f06be08bcfca22be7
herdal06 <h.erdal721@gmail.com>
2022-03-17 16:23:41 +0300

    Add local data set preparation

---
+++ app/src/main/java/com/example/programminglanguagesapp/Language.kt
@@ -0,0 +1,4 @@
+package com.example.programminglanguagesapp
+
+class Language {
+}
--- app/src/main/java/com/example/programminglanguagesapp/MainActivity.kt
+++ app/src/main/java/com/example/programminglanguagesapp/MainActivity.kt
@@ -7,5 +7,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // data preparation
+        var languageNames = ArrayList<String>()
+        languageNames.add("C")
+        languageNames.add("C++")
+        languageNames.add("Java")
+        languageNames.add("Kotlin")
+        languageNames.add("JavaScript")
+        languageNames.add("Go")
+        languageNames.add("Python")
+
+        // image id preparation
+        val cDrawableId = R.drawable.c
+        val cppDrawableId = R.drawable.cpp
+        val javaDrawableId = R.drawable.java
+        val kotlinDrawableId = R.drawable.kotlin
+        val javaScriptDrawableId = R.drawable.javascript
+        val goDrawableId = R.drawable.go
+        val pythonDrawableId = R.drawable.python
+
+        // adding drawable ids into an array
+        var languageDrawableList = ArrayList<Int>()
+        languageDrawableList.add(cDrawableId)
+        languageDrawableList.add(cppDrawableId)
+        languageDrawableList.add(javaDrawableId)
+        languageDrawableList.add(kotlinDrawableId)
+        languageDrawableList.add(javaScriptDrawableId)
+        languageDrawableList.add(goDrawableId)
+        languageDrawableList.add(pythonDrawableId)
+
     }
 }
