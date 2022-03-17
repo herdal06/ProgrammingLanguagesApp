c213d0e6ee038e58a929853ddce1c512b8c5bd23
herdal06 <h.erdal721@gmail.com>
2022-03-17 16:29:20 +0300

    Add ViewBinding config

--- app/src/main/java/com/example/programminglanguagesapp/MainActivity.kt
+++ app/src/main/java/com/example/programminglanguagesapp/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.programminglanguagesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.programminglanguagesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         // data preparation
         var languageNames = ArrayList<String>()
